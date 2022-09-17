5be1f10d03f505681d38cc59042969f0280294aa
Falafel <tadgranite@gmail.com>
2022-09-16 23:04:54 -0500

    made 3d platformer offa miziziziz

--- Test1Code/Player.gd
+++ Test1Code/Player.gd
@@ -1,54 +1,66 @@
-extends KinematicBody2D
-
-const MOVE_SPEED = 500
-const JUMP_FORCE = 1000
-const GRAVITY = 50
-const MAX_FALL_SPEED = 1000
-
-onready var anim_player = $AnimationPlayer
-onready var sprite = $Sprite
-
+extends KinematicBody
+ 
+const MOVE_SPEED = 12
+const JUMP_FORCE = 30
+const GRAVITY = 0.98
+const MAX_FALL_SPEED = 30
+ 
+const H_LOOK_SENS = 1.0
+const V_LOOK_SENS = 1.0
+ 
+onready var cam = $CamBase
+onready var anim = $Graphics/AnimationPlayer
+ 
 var y_velo = 0
-var facing_right = false
-
+ 
+func _ready():
+	anim.get_animation("walk").set_loop(true)
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+ 
+func _input(event):
+	if event is InputEventMouseMotion:
+		cam.rotation_degrees.x -= event.relative.y * V_LOOK_SENS
+		cam.rotation_degrees.x = clamp(cam.rotation_degrees.x, -90, 90)
+		rotation_degrees.y -= event.relative.x * H_LOOK_SENS
+ 
 func _physics_process(delta):
-	var move_dir = 0
+	var move_vec = Vector3()
+	if Input.is_action_pressed("move_forwards"):
+		move_vec.z -= 1
+	if Input.is_action_pressed("move_backwards"):
+		move_vec.z += 1
 	if Input.is_action_pressed("move_right"):
-		move_dir += 1
+		move_vec.x += 1
 	if Input.is_action_pressed("move_left"):
-		move_dir -= 1
-	move_and_slide(Vector2(move_dir * MOVE_SPEED, y_velo), Vector2(0, -1))
-	
+		move_vec.x -= 1
+	move_vec = move_vec.normalized()
+	move_vec = move_vec.rotated(Vector3(0, 1, 0), rotation.y)
+	move_vec *= MOVE_SPEED
+	move_vec.y = y_velo
+	move_and_slide(move_vec, Vector3(0, 1, 0))
+ 
 	var grounded = is_on_floor()
-	
-	y_velo += GRAVITY
+	y_velo -= GRAVITY
+	var just_jumped = false
 	if grounded and Input.is_action_just_pressed("jump"):
-		y_velo = -JUMP_FORCE
-	if grounded and y_velo >=5:
-		y_velo = 5
-	if y_velo > MAX_FALL_SPEED:
-		y_velo = MAX_FALL_SPEED
-		
-	if facing_right and move_dir < 0:
-		flip()
-	if !facing_right and move_dir > 0:
-		flip()
-		
-	if grounded:
-		if move_dir == 0:
+		just_jumped = true
+		y_velo = JUMP_FORCE
+	if grounded and y_velo <= 0:
+		y_velo = -0.1
+	if y_velo < -MAX_FALL_SPEED:
+		y_velo = -MAX_FALL_SPEED
+ 
+	if just_jumped:
+		play_anim("jump")
+	elif grounded:
+		if move_vec.x == 0 and move_vec.z == 0:
 			play_anim("idle")
 		else:
 			play_anim("walk")
-	else:
-		play_anim("jump")
-		
-	
-		
-func flip():
-	facing_right = !facing_right
-	sprite.flip_h = !sprite.flip_h
-	
-func play_anim(anim_name):
-	if anim_player.is_playing() and anim_player.current_animation == anim_name:
+ 
+func play_anim(name):
+	if anim.current_animation == name:
 		return
-	anim_player.play(anim_name)
+	anim.play(name)
+		
+		
