55c9a0b5c70020983eb02ffd502282b591943ee3
Falafel <tadgranite@gmail.com>
2022-09-17 14:55:57 -0500

    followed miziziz tutorial

--- Test1Code/Player.gd
+++ Test1Code/Player.gd
@@ -1,54 +1,45 @@
-extends KinematicBody2D
-
-const MOVE_SPEED = 500
-const JUMP_FORCE = 1000
-const GRAVITY = 50
-const MAX_FALL_SPEED = 1000
-
+extends KinematicBody
+ 
+const MOVE_SPEED = 4
+const MOUSE_SENS = 0.5
+ 
 onready var anim_player = $AnimationPlayer
-onready var sprite = $Sprite
-
-var y_velo = 0
-var facing_right = false
-
+onready var raycast = $RayCast
+ 
+func _ready():
+	#Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	yield(get_tree(), "idle_frame")
+	get_tree().call_group("zombies", "set_player", self)
+ 
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotation_degrees.y -= MOUSE_SENS * event.relative.x
+ 
+func _process(delta):
+	if Input.is_action_pressed("exit"):
+		get_tree().quit()
+	if Input.is_action_pressed("restart"):
+		kill()
+ 
 func _physics_process(delta):
-	var move_dir = 0
-	if Input.is_action_pressed("move_right"):
-		move_dir += 1
+	var move_vec = Vector3()
+	if Input.is_action_pressed("move_forwards"):
+		move_vec.z -= 1
+	if Input.is_action_pressed("move_backwards"):
+		move_vec.z += 1
 	if Input.is_action_pressed("move_left"):
-		move_dir -= 1
-	move_and_slide(Vector2(move_dir * MOVE_SPEED, y_velo), Vector2(0, -1))
-	
-	var grounded = is_on_floor()
-	
-	y_velo += GRAVITY
-	if grounded and Input.is_action_just_pressed("jump"):
-		y_velo = -JUMP_FORCE
-	if grounded and y_velo >=5:
-		y_velo = 5
-	if y_velo > MAX_FALL_SPEED:
-		y_velo = MAX_FALL_SPEED
-		
-	if facing_right and move_dir < 0:
-		flip()
-	if !facing_right and move_dir > 0:
-		flip()
-		
-	if grounded:
-		if move_dir == 0:
-			play_anim("idle")
-		else:
-			play_anim("walk")
-	else:
-		play_anim("jump")
-		
-	
-		
-func flip():
-	facing_right = !facing_right
-	sprite.flip_h = !sprite.flip_h
-	
-func play_anim(anim_name):
-	if anim_player.is_playing() and anim_player.current_animation == anim_name:
-		return
-	anim_player.play(anim_name)
+		move_vec.x -= 1
+	if Input.is_action_pressed("move_right"):
+		move_vec.x += 1
+	move_vec = move_vec.normalized()
+	move_vec = move_vec.rotated(Vector3(0, 1, 0), rotation.y)
+	move_and_collide(move_vec * MOVE_SPEED * delta)
+ 
+	if Input.is_action_pressed("shoot") and !anim_player.is_playing():
+		anim_player.play("shoot")
+		var coll = raycast.get_collider()
+		if raycast.is_colliding() and coll.has_method("kill"):
+			coll.kill()
+ 
+func kill():
+	get_tree().reload_current_scene()
