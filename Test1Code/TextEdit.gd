ca322fd6425ffc237576d0a86d671cf48c35c3fc
Tim Garnett <tadgranite@gmail.com>
2022-09-26 08:49:29 -0500

    a

--- Test1Code/TextEdit.gd
+++ Test1Code/TextEdit.gd
@@ -4,7 +4,7 @@ func _ready():
 	add_keyword_color("hello", Color(0.925781, 0.014465, 0.014465))
 	add_color_region('"','"', Color(0.014465, 0.925781, 0.818986))
 	add_color_region('//','', Color(0.522949, 0.53125, 0.530277), true)
-	
+	add_color_region("", "->", Color(0.522949, 0.53125, 0.530277), true)
 	var animals = ['dog', 'cat', 'bean']
 	
 	for i in animals:
