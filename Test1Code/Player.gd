35bde0011b89d753e8960e2d1d404d32d47eba70
Falafel <tadgranite@gmail.com>
2022-09-25 22:11:24 -0500

    added sprites

--- Test1Code/Player.gd
+++ Test1Code/Player.gd
@@ -5,16 +5,24 @@ const JUMP_FORCE = 1000
 const GRAVITY = 75
 const JUMP_BOOST = GRAVITY * 0.75
 const MAX_FALL_SPEED = 1000
+const DASH_LENGTH = 10
+const DASH_COOLDOWN = 50
+const DASH_SPEED = 2000
 
 onready var anim_player = $AnimationPlayer
 onready var Idle = $Idle
 onready var Jump_Down = $Jump_Down
 onready var Jump_Up = $Jump_Up
 onready var Run = $Run
+onready var Dash = $Dash
+onready var Slash = $Slash
 
+var x_velo = 0
 var y_velo = 0
 var facing_right = false
 var time_since_grounded = 0
+var time_since_dash = DASH_COOLDOWN
+
 
 func _physics_process(delta):
 	var move_dir = 0
@@ -22,12 +30,13 @@ func _physics_process(delta):
 		move_dir += 1
 	if Input.is_action_pressed("move_left"):
 		move_dir -= 1
-	move_and_slide(Vector2(move_dir * MOVE_SPEED, y_velo), Vector2(0, -1))
+	move_and_slide(Vector2(x_velo, y_velo), Vector2(0, -1))
 	
 	var grounded = is_on_floor()
 	
 	if is_on_ceiling():
 		y_velo = 0
+	
 	
 	y_velo += GRAVITY
 	if grounded and Input.is_action_just_pressed("jump"):
@@ -39,26 +48,41 @@ func _physics_process(delta):
 	if y_velo > MAX_FALL_SPEED:
 		y_velo = MAX_FALL_SPEED
 		
-	if facing_right and move_dir < 0:
-		flip()
-	if !facing_right and move_dir > 0:
-		flip()
+	if time_since_dash >= DASH_LENGTH:
+		if facing_right and move_dir > 0:
+			flip()
+		if !facing_right and move_dir < 0:
+			flip()
 		
-	if grounded:
-		time_since_grounded = 0
-		if move_dir == 0:
-			play_anim("Idle")
+		if grounded:
+			time_since_grounded = 0
+			if move_dir == 0:
+				play_anim("Idle")
+			else:
+				play_anim("Run")
 		else:
-			play_anim("Run")
+			time_since_grounded += 1
+			if y_velo > 0:
+				play_anim("Jump_Up")
+			else:
+				play_anim("Jump_Down")
+		x_velo = move_dir * MOVE_SPEED
 	else:
-		time_since_grounded += 1
-		if y_velo > 0:
-			play_anim("Jump_Up")
+		x_velo = move_dir * DASH_SPEED
+		play_anim("Dash")
+	
+	if time_since_dash >= DASH_COOLDOWN and Input.is_action_just_pressed("dash"):
+		time_since_dash = 0
+		if move_dir > 0:
+			x_velo += DASH_SPEED
+		elif move_dir < 0:
+			x_velo -= DASH_SPEED
+		elif Input.is_action_pressed("jump"):
+			y_velo -= DASH_SPEED*0.5
 		else:
-			play_anim("Jump_Down")
+			y_velo += DASH_SPEED*2
 	
-		
-	
+	time_since_dash += 1
 		
 func flip():
 	facing_right = !facing_right
@@ -66,6 +90,8 @@ func flip():
 	Jump_Down.flip_h = !Jump_Down.flip_h
 	Jump_Up.flip_h = !Jump_Up.flip_h
 	Run.flip_h = !Run.flip_h
+	Dash.flip_h = !Dash.flip_h
+	Slash.flip_h = !Slash.flip_h
 	
 func play_anim(anim_name):
 	if anim_player.is_playing() and anim_player.current_animation == anim_name:
@@ -79,3 +105,5 @@ func hide_all():
 	Jump_Down.hide()
 	Jump_Up.hide()
 	Run.hide()
+	Dash.hide()
+	Slash.hide()
