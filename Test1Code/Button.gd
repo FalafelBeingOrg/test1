408da75e3d58af20001548eab891ff06ecdac7df
Falafel <tadgranite@gmail.com>
2022-09-29 21:09:03 -0500

    added spaghetti code to make a search function

--- Test1Code/Button.gd
+++ Test1Code/Button.gd
@@ -1,19 +1,40 @@
 extends Button
 
+onready var Text = $"/root/Game/TextEdit"
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var pos = 0
+var is_flipped = false
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready() -> void:
 	pass # Replace with function body.
+
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_parent().remove_child(self)
 	
-func _pressed():
-	get_tree().change_scene("res://Game.tscn")
-
-
+	if Input.is_action_just_pressed("move_left"):
+		pos -= 1
+		is_flipped = false
+	elif Input.is_action_just_pressed("move_right"):
+		pos += 1
+		is_flipped = false
+		
+	if pos < 0:
+		pos = Text.befores.size()-1
+	if pos > Text.befores.size()-1:
+		pos = 0
+	print(Text.befores)
+	
+	if is_flipped:
+		text = Text.afters[pos]
+	else:
+		text = Text.befores[pos]
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
+#func _process(delta: float) -> void:
 #	pass
+
+func _on_Button_pressed() -> void:
+	is_flipped = !is_flipped
+
---
+++ Test1Code/SearchEdit.gd
@@ -0,0 +1,36 @@
+extends TextEdit
+
+onready var line_edit = get_parent().get_node("LineEdit")
+onready var Text = $"/root/Game/TextEdit"
+var b = 0
+var found = false
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	text = ""
+
+func _process(delta: float) -> void:
+	print(Text.name)
+
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_parent().remove_child(self)
+	
+	b = 0
+	found = false
+	text = ""
+	for i in Text.befores:
+		if line_edit.text == "":
+			print("line edit ''")
+		if i.find(line_edit.text) != -1 or line_edit.text == "":
+			text = text + "\n" + Text.befores[b] + "\n" + Text.afters[b]
+			found = true
+		b += 1
+	if !found:
+		text = "Not Found"
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
