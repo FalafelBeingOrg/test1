67a84beb0cd87d2c7e3e9a8e1e5fa22705cc25bd
Falafel <tadgranite@gmail.com>
2022-10-11 19:54:17 -0500

    added level select screen

---
+++ Test1Code/LevelHandler.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+const levels = [preload("res://World.tscn")]
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func change_level(level):
+	get_tree().change_scene_to(levels[level-1])
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
