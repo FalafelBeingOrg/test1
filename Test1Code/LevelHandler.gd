ebf9d99e98c38f498616e0f5857e6fc6d8f0ae34
Falafel <tadgranite@gmail.com>
2022-10-11 20:14:15 -0500

    added player movement

--- Test1Code/LevelHandler.gd
+++ Test1Code/LevelHandler.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-const levels = [preload("res://World.tscn")]
+const levels = [preload("res://LevelSelect.tscn"), preload("res://World.tscn")]
 # Declare member variables here. Examples:
 # var a: int = 2
 # var b: String = "text"
@@ -11,7 +11,7 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 func change_level(level):
-	get_tree().change_scene_to(levels[level-1])
+	get_tree().change_scene_to(levels[level])
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
 #	pass
---
+++ Test1Code/Player.gd
@@ -0,0 +1,34 @@
+extends KinematicBody2D
+
+const SPEED = 250
+
+onready var level_handler = get_node("/root/LevelHandler")
+
+var movement := Vector2.ZERO
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _physics_process(delta: float) -> void:
+	move()
+	
+func move():
+	movement = Vector2.ZERO
+	
+	if Input.is_action_pressed("ui_down"):
+		movement.y += SPEED
+	if Input.is_action_pressed("ui_up"):
+		movement.y -= SPEED
+	if Input.is_action_pressed("ui_right"):
+		movement.x += SPEED
+	if Input.is_action_pressed("ui_left"):
+		movement.x -= SPEED
+	
+	move_and_slide(movement, Vector2.UP)
+	
+func die():
+	LevelHandler.change_level(0)
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
